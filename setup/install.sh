b955e43513213fcd27c224a2add051bccd2e5efd
Roman E <69346184+EggertsIT@users.noreply.github.com>
2023-10-19 21:30:05 +0200

    Update install.sh
    Fixed Bug on Ubutnu22.04 not loading syslog-ng config

--- setup/install.sh
+++ setup/install.sh
@@ -18,16 +18,13 @@ systemctl stop loki
 systemctl stop promtail
 cat > /etc/loki/config.yml << EOF
 auth_enabled: false
-
 server:
   http_listen_port: 3100
   grpc_listen_port: 9096
   http_server_write_timeout: 310s
   http_server_read_timeout: 310s
-
 ingester:
   chunk_encoding: snappy
-
 common:
   instance_addr: 127.0.0.1
   path_prefix: /tmp/loki
@@ -39,12 +36,6 @@ common:
   ring:
     kvstore:
       store: inmemory
-
-
-
-
-
-
 querier:
   max_concurrent: 8
   query_timeout: 300s
@@ -73,7 +64,6 @@ frontend_worker:
   match_max_concurrent: true
 ruler:
   alertmanager_url: http://127.0.0.1:9093
-
 analytics:
   reporting_enabled: false
 EOF
@@ -114,7 +104,7 @@ scrape_configs:
         target_label: host
 EOF
 cat > /etc/syslog-ng/syslog-ng.conf << EOF
-@version: 3.38
+@version: 3.27
 @include "scl.conf"
 
 # Syslog-ng configuration file, compatible with default Debian syslogd
@@ -122,39 +112,34 @@ cat > /etc/syslog-ng/syslog-ng.conf << EOF
 
 # First, set some global options.
 options { chain_hostnames(off); flush_lines(0); use_dns(no); use_fqdn(no);
-	  dns_cache(no); owner("root"); group("adm"); perm(0640);
-	  stats_freq(0); bad_hostname("^gconfd$");
+          dns_cache(no); owner("root"); group("adm"); perm(0640);
+          stats_freq(0); bad_hostname("^gconfd$");
 };
 
 ########################
 # Sources
 ########################
-
+# This is the default behavior of sysklogd package
+# Logs may come from unix stream, but not from another machine.
+#
 source s_src {
        system();
        internal();
 };
-
 source s_net {
-        tcp(ip(0.0.0.0) port(514));
-        udp(ip(0.0.0.0) port(514));
+               tcp(ip(0.0.0.0) port(514));
+               udp(ip(0.0.0.0) port(514));
 };
-#source s_kmsg { file("/proc/kmsg");
-#};
+# If you wish to get logs from remote machine you should uncomment
+# this and comment the above source line.
+#
+#source s_net { tcp(ip(127.0.0.1) port(1000)); };
 
-
-########################
-# Destinations LOKI
-########################
-destination d_loki { syslog("localhost" transport("tcp") port(1514)); };
 ########################
 # Destinations
 ########################
 # First some standard logfile
 #
-
-
-#destination d_ufw { file("/var/log/ufw.log"); };
 destination d_auth { file("/var/log/auth.log"); };
 destination d_cron { file("/var/log/cron.log"); };
 destination d_daemon { file("/var/log/daemon.log"); };
@@ -164,7 +149,6 @@ destination d_mail { file("/var/log/mail.log"); };
 destination d_syslog { file("/var/log/syslog"); };
 destination d_user { file("/var/log/user.log"); };
 destination d_uucp { file("/var/log/uucp.log"); };
-
 
 # This files are the log come from the mail subsystem.
 #
@@ -190,7 +174,7 @@ destination d_console { usertty("root"); };
 
 # Virtual console.
 #
-destination d_console_all { getvirtconsole(); };
+destination d_console_all { file(`tty10`); };
 
 # The named pipe /dev/xconsole is for the nsole' utility.  To use it,
 # you must invoke nsole' with the -file' option:
@@ -209,10 +193,9 @@ destination d_ppp { file("/var/log/ppp.log"); };
 ########################
 # Filters
 ########################
-# Here's come the filter options. With this rules, we can set which
+# Here's come the filter options. With this rules, we can set which 
 # message go where.
 
-#filter f_ufw { match("\[UFW " value("MESSAGE")); };
 filter f_dbg { level(debug); };
 filter f_info { level(info); };
 filter f_notice { level(notice); };
@@ -222,26 +205,26 @@ filter f_crit { level(crit .. emerg); };
 
 filter f_debug { level(debug) and not facility(auth, authpriv, news, mail); };
 filter f_error { level(err .. emerg) ; };
-filter f_messages { level(info,notice,warn) and
+filter f_messages { level(info,notice,warn) and 
                     not facility(auth,authpriv,cron,daemon,mail,news); };
 
-filter f_auth { facility(auth, authpriv) and not filter(f_dbg); };
-filter f_cron { facility(cron) and not filter(f_dbg); };
-filter f_daemon { facility(daemon) and not filter(f_dbg); };
-filter f_kern { facility(kern) and not filter(f_dbg); };
-filter f_lpr { facility(lpr) and not filter(f_dbg); };
+filter f_auth { facility(auth, authpriv) and not filter(f_debug); };
+filter f_cron { facility(cron) and not filter(f_debug); };
+filter f_daemon { facility(daemon) and not filter(f_debug); };
+filter f_kern { facility(kern) and not filter(f_debug); };
+filter f_lpr { facility(lpr) and not filter(f_debug); };
 filter f_local { facility(local0, local1, local3, local4, local5,
-                        local6, local7) and not filter(f_dbg); };
-filter f_mail { facility(mail) and not filter(f_dbg); };
-filter f_news { facility(news) and not filter(f_dbg); };
-filter f_syslog3 { not facility(auth, authpriv, mail) and not filter(f_dbg); };
-filter f_user { facility(user) and not filter(f_dbg); };
-filter f_uucp { facility(uucp) and not filter(f_dbg); };
+                        local6, local7) and not filter(f_debug); };
+filter f_mail { facility(mail) and not filter(f_debug); };
+filter f_news { facility(news) and not filter(f_debug); };
+filter f_syslog3 { not facility(auth, authpriv, mail) and not filter(f_debug); };
+filter f_user { facility(user) and not filter(f_debug); };
+filter f_uucp { facility(uucp) and not filter(f_debug); };
 
 filter f_cnews { level(notice, err, crit) and facility(news); };
 filter f_cother { level(debug, info, notice, warn) or facility(daemon, mail); };
 
-filter f_ppp { facility(local2) and not filter(f_dbg); };
+filter f_ppp { facility(local2) and not filter(f_debug); };
 filter f_console { level(warn .. emerg); };
 
 ########################
@@ -257,21 +240,38 @@ log { source(s_src); filter(f_user); destination(d_user); };
 log { source(s_src); filter(f_uucp); destination(d_uucp); };
 
 log { source(s_src); filter(f_mail); destination(d_mail); };
+#log { source(s_src); filter(f_mail); filter(f_info); destination(d_mailinfo); };
+#log { source(s_src); filter(f_mail); filter(f_warn); destination(d_mailwarn); };
+#log { source(s_src); filter(f_mail); filter(f_err); destination(d_mailerr); };
 
 log { source(s_src); filter(f_news); filter(f_crit); destination(d_newscrit); };
 log { source(s_src); filter(f_news); filter(f_err); destination(d_newserr); };
 log { source(s_src); filter(f_news); filter(f_notice); destination(d_newsnotice); };
+#log { source(s_src); filter(f_cnews); destination(d_console_all); };
+#log { source(s_src); filter(f_cother); destination(d_console_all); };
+
+#log { source(s_src); filter(f_ppp); destination(d_ppp); };
 
 log { source(s_src); filter(f_debug); destination(d_debug); };
 log { source(s_src); filter(f_error); destination(d_error); };
 log { source(s_src); filter(f_messages); destination(d_messages); };
 
-log { source(s_src); filter(f_console); destination(d_console_all); destination(d_xconsole); };
+log { source(s_src); filter(f_console); destination(d_console_all);
+                                    destination(d_xconsole); };
 log { source(s_src); filter(f_crit); destination(d_console); };
-log { source(s_net); destination(d_loki); };
-#log { source(s_kmsg); filter(f_ufw); destination(d_ufw); };
 
-
+# All messages send to a remote site
+destination d_loki {
+        syslog("127.0.0.1" transport("tcp") port(1514));
+    };
+    
+log {
+	source(s_net);
+	destination(d_loki);
+};
+###
+# Include all config files in /etc/syslog-ng/conf.d/
+###
 @include "/etc/syslog-ng/conf.d/*.conf"
 EOF
 systemctl start loki
